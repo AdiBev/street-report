a80b44ebc082a3c54655e2bb1d41d7e2475e5785
Aditya Bevoor <adityabev@gmail.com>
2022-12-01 18:16:26 +0000

    Add map component styles and tests

---
+++ src/components/ol/OlMap.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import OSM from "ol/source/OSM";
+import { OlLayers } from "./OlLayers";
+import { OlMap } from "./OlMap";
+import { OlTileLayer } from "./OlTileLayer";
+
+describe("OlMap", () => {
+  it("renders map", () => {
+    render(
+      <OlMap>
+        <OlLayers>
+          <OlTileLayer source={new OSM()} />
+        </OlLayers>
+      </OlMap>
+    );
+    expect(screen.getByTestId("map-container")).toBeInTheDocument();
+  });
+});
--- src/components/ol/OlMap.tsx
+++ src/components/ol/OlMap.tsx
@@ -1,5 +1,43 @@
 import { Map, View } from "ol";
-import { createContext, useEffect, useMemo, useRef, useState } from "react";
+import { useGeographic } from "ol/proj";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { OlMapContext } from "../../context/OlMapContext";
+import { defaults as defaultControls } from "ol/control";
+import styled from "styled-components";
+
+const StyledMapWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+
+  //Control styles
+  .ol-overlaycontainer-stopevent {
+    // Zoom control styles
+    .ol-zoom {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      position: absolute;
+      bottom: 3rem;
+      right: 3rem;
+      background-color: white;
+      width: 2rem;
+      height: max-content;
+      padding: 1.5rem;
+
+      button {
+        width: 2.5rem;
+        height: 2.5rem;
+        font-size: 1.5rem;
+        border: unset;
+
+        &:last-child {
+          margin-top: 0.5rem;
+        }
+      }
+    }
+  }
+`;
 
 type OlMapProps = {
   children: React.ReactNode;
@@ -7,11 +45,8 @@ type OlMapProps = {
   center?: [number, number];
 };
 
-type OlMapContextT = {
-  map: Map | undefined;
-};
-
-export const OlMapContext = createContext<OlMapContextT | null>(null);
+const DEFAULT_ZOOM = 15;
+const DEFAULT_CENTER = [-0.189115, 51.502982];
 
 export const OlMap: React.FunctionComponent<OlMapProps> = ({
   children,
@@ -20,16 +55,20 @@ export const OlMap: React.FunctionComponent<OlMapProps> = ({
 }) => {
   const [olMap, setOlMap] = useState<Map>();
   const mapRef = useRef<HTMLDivElement>(null);
+
+  // importing hook to use LAT/LON values
+  useGeographic();
+
   const memoizedViewOptions = useMemo(
-    () => ({ zoom: 5, center: [53.388504070398064, -2.9805909170326585] }),
-    [zoom]
+    () => ({ zoom: zoom ?? DEFAULT_ZOOM, center: center ?? DEFAULT_CENTER }),
+    [center, zoom]
   );
 
   useEffect(() => {
     const mapOptions = {
       view: new View(memoizedViewOptions),
       layers: [],
-      controls: [],
+      controls: defaultControls({ attribution: false, rotate: false }),
       overlays: [],
     };
     const olMap = new Map(mapOptions);
@@ -40,9 +79,9 @@ export const OlMap: React.FunctionComponent<OlMapProps> = ({
 
   return (
     <OlMapContext.Provider value={{ map: olMap }}>
-      <div ref={mapRef} style={{ width: "100%", height: "100%" }}>
+      <StyledMapWrapper ref={mapRef} data-testid="map-container">
         {children}
-      </div>
+      </StyledMapWrapper>
     </OlMapContext.Provider>
   );
 };
