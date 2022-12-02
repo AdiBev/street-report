779b2fa9be588a8519510cc6ca7da3d3ccb8fd1b
Aditya Bevoor <adityabev@gmail.com>
2022-12-02 12:48:49 +0000

    Remove ol components

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { OlMap } from "./components/ol/OlMap";
-import OSM from "ol/source/OSM";
-import { OlTileLayer } from "./components/ol/OlTileLayer";
-import { useGeographic } from "ol/proj";
-
 function App() {
-  useGeographic();
-  return (
-    <div className="App" style={{ width: "100vw", height: "100vh" }}>
-      <OlMap>
-        <OlTileLayer source={new OSM()} />
-      </OlMap>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
