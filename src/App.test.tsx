c87268581e5c06f3e3ff8eb779932637861c09de
Aditya Bevoor <adityabev@gmail.com>
2022-12-05 23:07:11 +0000

    Fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,5 +4,5 @@ import App from "./App"
 
 test("renders learn react link", () => {
   render(<App />)
-  expect(screen.getByText("Hllo")).toBeInTheDocument()
+  expect(screen.getByText("Hello")).toBeInTheDocument()
 })
