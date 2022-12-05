23376f1f7d7e5159aa743ae2a04c7f4d22d3e0c7
Aditya Bevoor <adityabev@gmail.com>
2022-12-05 23:02:24 +0000

    Fail test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,5 +4,5 @@ import App from "./App"
 
 test("renders learn react link", () => {
   render(<App />)
-  expect(screen.getByText("Hello")).toBeInTheDocument()
+  expect(screen.getByText("Hllo")).toBeInTheDocument()
 })
